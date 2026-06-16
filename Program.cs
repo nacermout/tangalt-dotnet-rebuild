29e5997aeb8609639dbbd365d5444535d9325753
Aderghal <indiphai@gmail.com>
2026-06-16 12:38:37 -0400

    Fix: PORT pour Railway

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("TangaltFrontend", policy =>
